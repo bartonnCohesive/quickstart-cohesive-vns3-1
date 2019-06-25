9af8e6d9b5e580083009b1b53a55825cc0f1c8e3
Barton E Nicholls <barton.nicholls@cohesive.net>
2019-06-25 01:40:07 -0400

    Create client-bootstrap-c.sh

---
+++ scripts/client-bootstrap-c.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#Waits for openvpn service to become active and installs Cohesive Networks VNS3 Routing Agent
+
+IP=$(echo $VNS3OVERLAYIP | sed 's/_/\./g')
+
+ wait_for_openvpn () {
+   while :
+    do
+    servicestatus=`/sbin/ifconfig | grep "$IP" 2>&1`
+          if [ $? = 0 ] ; then
+           sudo rpm -ivh /tmp/cohesive-ra-1.1.1_x86_64.rpm;
+           exit 0
+          fi
+        sleep 2
+    done
+}
+
+wait_for_openvpn
